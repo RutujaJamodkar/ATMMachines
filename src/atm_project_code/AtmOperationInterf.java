f30eb5e42988a4bd6878fec37682efa48c4d4ab2
rutuj <rutuj@RutujaJamodkar>
2023-05-23 19:17:51 +0530

    First commit

---
+++ src/atm_project_code/AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package atm_project_code;
+
+public interface AtmOperationInterf {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
